1c98a6833ba4cf5129369f342bd7e7ccf141ae59
mathk32 <matheuskennedyt@gmail.com>
2026-06-08 23:53:23 -0300

    feat(scripts): implement scale, rotation, and multi-material color logic

--- src/project-ar/Assets/Scripts/ModelController.cs
+++ src/project-ar/Assets/Scripts/ModelController.cs
@@ -13,17 +13,38 @@ public class ModelController : MonoBehaviour
 
     public void SetScale(float scaleValue)
     {
-
+        if (modelTransform != null)
+        {
+            modelTransform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+        }
     }
 
     public void RotateModel(float yRotation)
     {
-
+        if (modelTransform != null)
+        {
+            modelTransform.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+        }
     }
 
     public void ChangeColor(Color newColor)
     {
-
+        if (modelRenderer != null)
+        {
+            foreach(Material mat in modelRenderer.materials)
+            {
+               mat.color = newColor;
+            }
+        }
     }
+
+    [ContextMenu("Test: Double Scale")]
+    private void TestScale() => SetScale(0.6f);
+
+    [ContextMenu("Test: Rotate 45 degrees")]
+    private void TestRotation() => RotateModel(45f);
+
+    [ContextMenu("Test: Turn Blue")]
+    private void TestColor() => ChangeColor(Color.blue);
 
 }
